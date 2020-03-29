58ee742d5c63f37070697274da90814cb5270d1e
suzumi <>
2020-03-29 12:41:13 +0900

    github草

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users #deviseコマンドで追加される
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "top#index" #to:は省略可能
 end
