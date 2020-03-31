180bb333d739e18a632664eed6f724bb81b37efb
suzumi <otsuka.suzumi@gmail.com>
2020-03-31 16:34:31 +0900

    ユーザーにグループ名を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  #accessor
+  attr_accessor :group_key
 end
